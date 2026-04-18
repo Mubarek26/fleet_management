72ee2ca50f049de246fab1a4c6569dd83c6118d0
mubarek <mezid58@gmail.com>
2026-04-18 12:35:25 +0300

    update

--- src/app.js
+++ src/app.js
@@ -50,6 +50,7 @@ const allowedOrigins = Array.from(new Set([
 	"http://localhost:3000",
 	"http://localhost:5000",
 	"http://localhost:8000",
+	"http://localhost:8080",
 	"http://localhost:5173",
 	"http://localhost:5174",
 	"http://127.0.0.1:5173",
